4248f620f303a5fea09acf4354560f3964f6ec16
muneeb3123 <rehmanmuneeb66@gmail.com>
2023-07-15 11:52:56 +0500

    setup main parent class

---
+++ .github/workflows/modules/item.rb
@@ -0,0 +1,23 @@
+require 'date'
+
+class Item
+  attr_accessor :publish_date
+
+  def initialize(publish_date)
+    @id = rand(1..100)
+    @publish_date = Date.parse(publish_date)
+    @archived = false
+  end
+
+
+  def move_to_archived
+    @archived = can_be_archived?
+  end
+
+  private
+
+  def can_be_archived?
+    (Date.today - @publish_date).to_i > (10 * 365)
+  end
+
+end
