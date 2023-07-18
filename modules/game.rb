a9397280fdede8b3e7cb04842df71c72a18f9788
Larry Ivan Villegas Costas <larry_villegas@hotmail.com>
2023-07-18 18:30:10 -0400

    Add unit test for Game class

--- modules/game.rb
+++ modules/game.rb
@@ -1,13 +1,14 @@
+require 'date'
 require_relative 'item'
 
-class Game < item
+class Game < Item
   attr_accessor :multiplayer, :last_played_at
 
   def initialize(publish_date, multiplayer, last_played_at, id: rand(1..1000))
     super(publish_date)
     @id = id
     @multiplayer = multiplayer
-    @last_played_at = last_played_at
+    @last_played_at = Date.parse(last_played_at)
   end
 
   def can_be_archived?
