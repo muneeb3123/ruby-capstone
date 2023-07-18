19ca4e7b973b99967dcf75595087786d164d64f0
Larry Ivan Villegas Costas <larry_villegas@hotmail.com>
2023-07-18 12:48:45 -0400

    Add game class and its methods

---
+++ modules/game.rb
@@ -0,0 +1,16 @@
+require_relative 'item'
+
+class Game < item
+  attr_accessor :multiplayer, :last_played_at
+
+  def initialize(publish_date, multiplayer, last_played_at, id: rand(1..1000))
+    super(publish_date)
+    @id = id
+    @multiplayer = multiplayer
+    @last_played_at = last_played_at
+  end
+
+  def can_be_archived?
+    super && (Date.today - @last_played_at).to_i > (2 * 365)
+  end
+end
