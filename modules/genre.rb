c4acb82e109087cff3976cb500a9ef206d5eb290
Osman Alhassan <oalhassan847@gmail.com>
2023-07-21 02:17:13 +0800

    Load genre from json data

--- modules/genre.rb
+++ modules/genre.rb
@@ -21,4 +21,17 @@ class Genre
       File.write('../json/genres.json', JSON.generate(json_data))
     end
   end
+
+  def load_genre(genres)
+    return unless File.exist?(genres)
+
+    json_data = JSON.parse(File.read('../json/genres.json'))
+    genre = Genre.new(json_data[genre], json_data['description'])
+    json_data['items'].each do |item_data|
+      item = Item.new(item_data['title'], Date.parse(item_data['published_date']))
+      item.genre = genre
+      genre.add_item(item)
+    end
+    genre
+  end
 end
