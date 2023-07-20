6a4b23260b7176abfbd70d9d08625ee4b35f893c
Osman Alhassan <oalhassan847@gmail.com>
2023-07-21 02:22:29 +0800

    Add load music

--- modules/music.rb
+++ modules/music.rb
@@ -27,4 +27,18 @@ class MusicAlbum < Item
       album.write(JSON.generate(json_data))
     end
   end
+
+  def load_music(music_album)
+    json_data = JSON.parse(File.read(music_album))
+    music_album = MusicAlbum.new(
+      json_data['title'],
+      Date.parse(json_data['published_date']),
+      json_data['artist'],
+      json_data['total_tracks'],
+      json_data['on_spotify']
+    )
+    genre_data = json_data['genre']
+    music_album.genre = genre_data.nil? ? nil : Genre.new(genre_data['name'], genre_data['description'])
+    music_album
+  end
 end
