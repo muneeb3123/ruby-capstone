2383c7bc803b4a335d42e4ebbb783c4a63beea87
Osman Alhassan <oalhassan847@gmail.com>
2023-07-21 01:42:52 +0800

    Fix linter errors

--- modules/music.rb
+++ modules/music.rb
@@ -3,7 +3,7 @@ require_relative 'item'
 class MusicAlbum < Item
   attr_accessor :artist, :total_tracks, :on_spotify
 
-  def initialize(title, published_date, artist, total_tracks, on_spotify)
+  def initialize(_title, published_date, artist, total_tracks, on_spotify)
     super(published_date)
     @artist = artist
     @total_tracks = total_tracks
